caac67fa0ddc451909c335e5cc2e67d513e147e4
Sezim <amangulovasezim@gmail.com>
2024-04-30 11:02:56 +0100

    papki s klassami, massiv

---
+++ src/components/description/Description.jsx
@@ -0,0 +1,10 @@
+import styles from "./description.module.css"
+
+export function Description ({obj}) {
+        return (
+            <div>
+                <h2 className={styles.titles}>{obj.title}</h2>
+                <p className={styles.descriptions}>{obj.description}</p>
+            </div>
+        );
+}
---
+++ src/pages/todosPage/TodosPage.jsx
@@ -0,0 +1,15 @@
+import {Todo} from "../../components/todo/Todo";
+import styles from "./todosPage.module.css"
+
+
+export function TodosPage () {
+        const arr = ["todo 1", "todo 2", "todo 3"];
+        return (
+            <div className={styles.description}>
+                {arr.map((item, index) => (
+                    <Todo key={index} item={item} />
+                ))}
+            </div>
+        );
+}
+
